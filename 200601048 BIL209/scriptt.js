c2950b1441ea82d1923b841d2fde702bf827b1c0
Buse Yiğit <80629779+bygt@users.noreply.github.com>
2021-10-07 19:57:48 +0300

    Add files via upload

---
+++ 200601048 BIL209/scriptt.js
@@ -0,0 +1,29 @@
+
+        function topla()
+        {
+            var val1 = parseInt (document.getElementById("Deger1").value);
+            var val2 = parseInt (document.getElementById("Deger2").value);
+            var cevap = document.getElementById("Cevap");
+            cevap.value = val1 + val2;
+        }
+        function cikar()
+        {
+            var val1 = parseInt (document.getElementById("Deger1").value);
+            var val2 = parseInt (document.getElementById("Deger2").value);
+            var cevap = document.getElementById("Cevap");
+            cevap.value = val1 - val2;
+        }
+        function carp()
+        {
+            var val1 = parseInt (document.getElementById("Deger1").value);
+            var val2 = parseInt (document.getElementById("Deger2").value);
+            var cevap = document.getElementById("Cevap");
+            cevap.value = val1 * val2;
+        }
+        function bol()
+        {
+            var val1 = parseInt (document.getElementById("Deger1").value);
+            var val2 = parseInt (document.getElementById("Deger2").value);
+            var cevap = document.getElementById("Cevap");
+            cevap.value = val1 / val2;
+        }
